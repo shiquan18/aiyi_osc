361fb9f3d3df58bb20f04c2687da535c94acdbd9
aiyi <609790701@qq.com>
2019-05-30 17:49:07 +0800

    网页页面

--- lib/pages/CommonWebPage.dart
+++ lib/pages/CommonWebPage.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 
 class CommonWebPage extends StatefulWidget {
   final String title;
@@ -11,12 +13,54 @@ class CommonWebPage extends StatefulWidget {
 }
 
 class _CommonWebPage extends State<CommonWebPage> {
+  bool loading = true;
+  final flutterWebViewPlugin = FlutterWebviewPlugin();
+
+  @override
+  void initState() {
+    super.initState();
+    flutterWebViewPlugin.onStateChanged.listen((state) {
+//      if (state.type == WebViewState.finishLoad) {
+//        // 加载完成
+//        setState(() {
+//          loading = false;
+//        });
+//      } else if (state.type == WebViewState.startLoad) {
+//        setState(() {
+//          loading = true;
+//        });
+//      }
+    });
+    flutterWebViewPlugin.onUrlChanged.listen((url) {
+      setState(() {
+        loading = false;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Text(
-      'CommonWebPage',
-      textAlign: TextAlign.center,
+    List<Widget> titleContent = [];
+    titleContent.add(Text(
+      widget.title,
+      style: TextStyle(color: Colors.white),
     ));
+    if (loading) {
+      titleContent.add(CupertinoActivityIndicator());
+    }
+    titleContent.add(Container(width: 50.0));
+    return WebviewScaffold(
+      url: widget.url,
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: titleContent,
+        ),
+        iconTheme: IconThemeData(color: Colors.white),
+      ),
+      withZoom: true,
+      withLocalStorage: true,
+      withJavascript: true,
+    );
   }
 }
